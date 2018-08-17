46711de3a02b855037ba50a6e2e1699a304399f3
WolfWhitaker <vladislavkiru@mail.ru>
2018-08-17 12:38:45 +0500

    docs: add copyright

--- MVC/src/main/java/com/github/wolfwhitaker/sitecrawler/mvc/exception/ModelException.java
+++ MVC/src/main/java/com/github/wolfwhitaker/sitecrawler/mvc/exception/ModelException.java
@@ -1,3 +1,27 @@
+/*
+ *
+ ****************************************************************************
+ *                                                                          *
+ *                                SiteCrawler                               *
+ *                                                                          *
+ * is an open source project, which is distributed under "fair use" terms.  *
+ * It means you can use any part of it's code as you wish and redistribute  *
+ * it, but you should mention the author.                                   *
+ *                                                                          *
+ * WARNING!!! It is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR  *
+ * CONDITIONS OF ANY KIND. You should also keep in mind that this project   *
+ * uses the code of the third-party developers. So, if you want to use some *
+ * part of it's code WHICH USES THIRD-PART LIBRARIES in your own project,   *
+ * MAKE SURE that the way you use it doesn't violate THEIR TERMS OF USE.    *
+ *                                                                          *
+ * Copyright (C) 2018 WolfWhitaker                                          *
+ *                                                                          *
+ * My github page: https://github.com/WolfWhitaker                          *
+ *                                                                          *
+ ****************************************************************************
+ *
+ */
+
 package com.github.wolfwhitaker.sitecrawler.mvc.exception;
 
  /** This class represents a general model exception. It should wrap any exception
